e0637a97ae18195b73d441c5b19b1f30838ab76d
JOSE-OMAR-FLORES <153056143+JOSE-OMAR-FLORES@users.noreply.github.com>
2025-08-16 19:12:17 -0600

    xx

--- src/pages/KitchenDisplay.tsx
+++ src/pages/KitchenDisplay.tsx
@@ -7,6 +7,7 @@ import { Clock, CheckCircle, AlertCircle, PlayCircle } from "lucide-react";
 import axios from 'axios';
 import { useAuth } from "@/context/AuthContext";
 import { useNavigate } from "react-router-dom";
+
 import { BACKEND_URL } from '../config';
 
 interface ApiOrderItem {
